dbbc11865aadcf0425339ce8b7890b838860d7fd
levanpon1009 <levanpon1009@gmail.com>
2020-01-20 19:57:59 +0700

    Fix model

--- train.py
+++ train.py
@@ -5,15 +5,22 @@ import tensorflow as tf
 from utils.config import AudioConfig, DatasetConfig
 from utils.dataset import DeepSpeechDataset, input_fn
 from utils import fn
-from model import *
+import model as deep_speech
+from absl import app as absl_app
+from absl import flags
 import decoder
+from official.utils.flags import core as flags_core
+from official.utils.logs import hooks_helper
+from official.utils.logs import logger
+from official.utils.misc import distribution_utils
+from official.utils.misc import model_helpers
 
 _BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 _DEFAULT_META_CSV = os.path.join(_BASE_DIR, 'meta.csv')
 _DEFAULT_CHARACTERS_FILE = os.path.join(_BASE_DIR, 'characters.txt')
 _DEFAULT_MODEL_DIR = os.path.join(_BASE_DIR, 'discriminative_model')
-_DEFAULT_TRAIN_DIR = "D:/Kaggle/data/speech_to_text_vietnamese/train/"
-_DEFAULT_EVAL_DIR = "D:/Kaggle/data/speech_to_text_vietnamese/test/"
+_DEFAULT_TRAIN_DIR = os.path.join(_BASE_DIR, "speech_to_text_vietnamese/train/")
+_DEFAULT_EVAL_DIR = os.path.join(_BASE_DIR, "speech_to_text_vietnamese/test/")
 # Evaluation metrics
 _WER_KEY = "WER"
 _CER_KEY = "CER"
@@ -33,20 +40,19 @@ def ctc_loss(label_length, ctc_input_length, labels, logits):
                                                    sequence_length=ctc_input_length), axis=1)
 
 
-def generate_dataset(data_dir, sample_rate,
-                     window_ms, stride_ms, characters_file, sortagrad):
-    audio_conf = AudioConfig(sample_rate=sample_rate,
-                             window_ms=window_ms,
-                             stride_ms=stride_ms,
+def generate_dataset(data_dir):
+    """Generate a speech dataset."""
+    audio_conf = AudioConfig(sample_rate=flags_obj.sample_rate,
+                             window_ms=flags_obj.window_ms,
+                             stride_ms=flags_obj.stride_ms,
                              normalize=True)
-    data_conf = DatasetConfig(
-        audio_config=audio_conf,
-        data_path=data_dir,
-        characters_file_path=characters_file,
-        sortagrad=sortagrad
+    train_data_conf = DatasetConfig(
+        audio_conf,
+        data_dir,
+        flags_obj.vocabulary_file,
+        flags_obj.sortagrad
     )
-
-    speech_dataset = DeepSpeechDataset(data_conf)
+    speech_dataset = DeepSpeechDataset(train_data_conf)
     return speech_dataset
 
 
@@ -117,22 +123,15 @@ def model_fn(features, labels, mode, params):
       current mode.
     """
     num_classes = params["num_classes"]
-    rnn_hidden_layers = params["rnn_hidden_layers"]
-    rnn_type = params["rnn_type"]
-    rnn_hidden_size = params["rnn_hidden_size"]
-    is_bidirectional = params["is_bidirectional"]
-    use_bias = params["use_bias"]
-    learning_rate = params["learning_rate"]
-
     input_length = features["input_length"]
     label_length = features["label_length"]
     features = features["features"]
 
     # Create DeepSpeech2 model.
-    model = DeepSpeech2(
-        rnn_hidden_layers, rnn_type,
-        is_bidirectional, rnn_hidden_size,
-        num_classes, use_bias)
+    model = deep_speech.DeepSpeech2(
+        flags_obj.rnn_hidden_layers, flags_obj.rnn_type,
+        flags_obj.is_bidirectional, flags_obj.rnn_hidden_size,
+        num_classes, flags_obj.use_bias)
 
     if mode == tf.estimator.ModeKeys.PREDICT:
         logits = model(features, training=False)
@@ -145,30 +144,21 @@ def model_fn(features, labels, mode, params):
             mode=mode,
             predictions=predictions)
 
-    print('In training mode.')
     logits = model(features, training=True)
     probs = tf.nn.softmax(logits)
     ctc_input_length = fn.compute_length_after_conv(
         tf.shape(features)[1], tf.shape(probs)[1], input_length)
-    print('# Compute CTC loss')
-    loss = tf.reduce_mean(ctc_loss(label_length, ctc_input_length, labels, probs))
+    # Compute CTC loss
+    loss = tf.reduce_mean(ctc_loss(
+        label_length, ctc_input_length, labels, probs))
 
-    optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
-    # global_step = tf.compat.v1.train.get_or_create_global_step()
-    # minimize_op = optimizer.minimize(loss, global_step=global_step)
-    # update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
-    # # Create the train_op that groups both minimize_ops and update_ops
-    # train_op = tf.group(minimize_op, update_ops)
+    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=flags_obj.learning_rate)
 
-    optimizer.iterations = tf.compat.v1.train.get_or_create_global_step()
-    # Get both the unconditional updates (the None part)
-    # and the input-conditional updates (the features part).
-    update_ops = model.get_updates_for(None) + model.get_updates_for(features)
+    global_step = tf.compat.v1.train.get_or_create_global_step()
+    minimize_op = optimizer.minimize(loss, global_step=global_step)
 
-    minimize_op = optimizer.get_updates(
-        loss,
-        model.trainable_variables)[0]
-    train_op = tf.group(minimize_op, *update_ops)
+    update_ops = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.UPDATE_OPS)
+    train_op = tf.group(minimize_op, update_ops)
 
     return tf.estimator.EstimatorSpec(
         mode=mode,
@@ -176,126 +166,191 @@ def model_fn(features, labels, mode, params):
         train_op=train_op)
 
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('--train-dir', type=str, dest='train_dir',
-                        default=_DEFAULT_TRAIN_DIR,
-                        help='')
-    parser.add_argument('--eval-dir', type=str, dest='eval_dir',
-                        default=_DEFAULT_EVAL_DIR,
-                        help='')
-    parser.add_argument('-sr', '--sample-rate', type=int, dest='sample_rate',
-                        default=16000,
-                        help='The sample rate for audio')
-    parser.add_argument('--window-ms', type=int, default=20, dest='window_ms',
-                        help='The frame length for spectrogram')
-    parser.add_argument('--stride-ms', type=int, default=10, dest='stride_ms',
-                        help='The frame step')
-    parser.add_argument('--characters-file', type=str, dest='characters_file',
-                        default=_DEFAULT_CHARACTERS_FILE,
-                        help='The file path of characters file')
-    parser.add_argument('--model-dir', type=str, dest='model_dir',
-                        default=_DEFAULT_MODEL_DIR,
-                        help='The file path of characters file')
-    parser.add_argument('--rnn-hidden-size', type=int, default=800, dest='rnn_hidden_size',
-                        help='The hidden size of RNNs.')
-    parser.add_argument('--rnn-hidden-layers', type=int, default=5, dest='rnn_hidden_layers',
-                        help='The number of RNN layers.')
-    parser.add_argument('--use-bias', type=bool, default=True, dest='use_bias',
-                        help='Use bias in the last fully-connected layer')
-    parser.add_argument('--is-bidirectional', type=bool, default=True, dest='is_bidirectional',
-                        help='If rnn unit is bidirectional')
-    parser.add_argument('--rnn-type', type=str, choices=('lstm', 'gru'), default='gru', dest='rnn_type',
-                        help='Type of RNN cell.')
-    parser.add_argument('-lr', '--learning-rate', type=float,
-                        default=5e-4, dest='learning_rate',
-                        help='The initial learning rate.')
-    parser.add_argument('--batch-size', type=int,
-                        default=16, dest='batch_size',
-                        help='The initial batch size.')
-    parser.add_argument('--train-epochs', type=int,
-                        default=100, dest='train_epochs',
-                        help='The initial train epochs')
-    parser.add_argument('--epochs-between-evals', type=int,
-                        default=2, dest='epochs_between_evals',
-                        help='The initial train epochs')
-    parser.add_argument('--sortagrad', type=bool, default=True, dest='sortagrad',
-                        help='If true, sort examples by audio length and perform no '
-                             'batch_wise shuffling for the first epoch.')
-    parser.add_argument('--wer_threshold', default=None, dest='wer_threshold',
-                        help='If passed, training will stop when the evaluation metric WER is  '
-                             'greater than or equal to wer_threshold. For libri speech dataset '
-                             'the desired wer_threshold is 0.23 which is the result achieved by '
-                             ' MLPerf implementation.')
-
-    return parser.parse_args()
-
-
-def main(args):
+def run(_):
+    tf.compat.v1.set_random_seed(flags_obj.seed)
     print("Data pre-processing...")
-    train_dataset = generate_dataset(args.train_dir,
-                                     args.sample_rate,
-                                     args.window_ms,
-                                     args.stride_ms,
-                                     args.characters_file,
-                                     args.sortagrad)
-    eval_dataset = generate_dataset(args.eval_dir,
-                                    args.sample_rate,
-                                    args.window_ms,
-                                    args.stride_ms,
-                                    args.characters_file,
-                                    args.sortagrad)
-    num_classes = len(train_dataset.speech_labels)
+    train_speech_dataset = generate_dataset(flags_obj.train_data_dir)
+    eval_speech_dataset = generate_dataset(flags_obj.eval_data_dir)
+    num_classes = len(train_speech_dataset.speech_labels)
 
-    strategy = tf.distribute.OneDeviceStrategy(device="/gpu:0")
-    config = tf.estimator.RunConfig(train_distribute=strategy)
+    num_gpus = flags_core.get_num_gpus(flags_obj)
+    distribution_strategy = distribution_utils.get_distribution_strategy(num_gpus=num_gpus)
+    run_config = tf.estimator.RunConfig(
+        train_distribute=distribution_strategy)
 
     estimator = tf.estimator.Estimator(
         model_fn=model_fn,
-        model_dir=args.model_dir,
-        config=config,
+        model_dir=flags_obj.model_dir,
+        config=run_config,
         params={
             "num_classes": num_classes,
-            "rnn_hidden_layers": args.rnn_hidden_layers,
-            "rnn_type": args.rnn_type,
-            "rnn_hidden_size": args.rnn_hidden_size,
-            "is_bidirectional": args.is_bidirectional,
-            "use_bias": args.use_bias,
-            "learning_rate": args.learning_rate,
         }
     )
 
-    per_replica_batch_size = fn.per_replica_batch_size(args.batch_size, 1)
+    run_params = {
+        "batch_size": flags_obj.batch_size,
+        "train_epochs": flags_obj.train_epochs,
+        "rnn_hidden_size": flags_obj.rnn_hidden_size,
+        "rnn_hidden_layers": flags_obj.rnn_hidden_layers,
+        "rnn_type": flags_obj.rnn_type,
+        "is_bidirectional": flags_obj.is_bidirectional,
+        "use_bias": flags_obj.use_bias
+    }
+
+    dataset_name = "LibriSpeech"
+    benchmark_logger = logger.get_benchmark_logger()
+    benchmark_logger.log_run_info("deep_speech", dataset_name, run_params,
+                                  test_id=flags_obj.benchmark_test_id)
+
+    train_hooks = hooks_helper.get_train_hooks(
+        flags_obj.hooks,
+        model_dir=flags_obj.model_dir,
+        batch_size=flags_obj.batch_size)
+
+    per_replica_batch_size = distribution_utils.per_replica_batch_size(
+        flags_obj.batch_size, num_gpus)
 
     def input_fn_train():
-        return input_fn(per_replica_batch_size, train_dataset)
+        return input_fn(per_replica_batch_size, train_speech_dataset)
 
     def input_fn_eval():
-        return input_fn(per_replica_batch_size, eval_dataset)
+        return input_fn(
+            per_replica_batch_size, eval_speech_dataset)
 
-    total_training_cycle = (args.train_epochs // args.epochs_between_evals)
+    total_training_cycle = (flags_obj.train_epochs //
+                            flags_obj.epochs_between_evals)
 
     for cycle_index in range(total_training_cycle):
         print('Starting a training cycle: %d/%d' % (cycle_index + 1, total_training_cycle))
 
-        train_dataset.entries = fn.batch_wise_dataset_shuffle(
-            train_dataset.entries, cycle_index, args.sortagrad,
-            args.batch_size)
+        train_speech_dataset.entries = fn.batch_wise_dataset_shuffle(
+            train_speech_dataset.entries, cycle_index, flags_obj.sortagrad,
+            flags_obj.batch_size)
 
-        estimator.train(input_fn=input_fn_train)
+        estimator.train(input_fn=input_fn_train, hooks=train_hooks)
 
         eval_results = evaluate_model(
-            estimator, eval_dataset.speech_labels,
-            eval_dataset.entries, input_fn_eval)
+            estimator, eval_speech_dataset.speech_labels,
+            eval_speech_dataset.entries, input_fn_eval)
 
         print("Iteration {}: WER = {:.2f}, CER = {:.2f}".format(
             cycle_index + 1, eval_results[_WER_KEY], eval_results[_CER_KEY]))
 
-        if fn.past_stop_threshold(
-                args.wer_threshold, eval_results[_WER_KEY]):
+        if model_helpers.past_stop_threshold(
+                flags_obj.wer_threshold, eval_results[_WER_KEY]):
             break
 
 
-if __name__ == '__main__':
-    main(parse_args())
+def define_deep_speech_flags():
+    """Add flags for run_deep_speech."""
+    # Add common flags
+    flags_core.define_base(
+        data_dir=False,  # we use train_data_dir and eval_data_dir instead
+        export_dir=True,
+        train_epochs=True,
+        hooks=True,
+        epochs_between_evals=True,
+    )
+    flags_core.define_performance(
+        num_parallel_calls=False,
+        inter_op=False,
+        intra_op=False,
+        synthetic_data=False,
+        max_train_steps=False,
+        dtype=False
+    )
+    flags_core.define_benchmark()
+    flags.adopt_module_key_flags(flags_core)
+
+    flags_core.set_defaults(
+        model_dir="/tmp/deep_speech_model/",
+        export_dir="/tmp/deep_speech_saved_model/",
+        train_epochs=10,
+        batch_size=128,
+        hooks=[],
+        epochs_between_evals=4)
+
+    # Deep speech flags
+    flags.DEFINE_integer(
+        name="seed", default=1,
+        help=flags_core.help_wrap("The random seed."))
+
+    flags.DEFINE_string(
+        name="train_data_dir",
+        default=_DEFAULT_TRAIN_DIR,
+        help=flags_core.help_wrap("The csv file path of train dataset."))
+
+    flags.DEFINE_string(
+        name="eval_data_dir",
+        default=_DEFAULT_EVAL_DIR,
+        help=flags_core.help_wrap("The csv file path of evaluation dataset."))
+
+    flags.DEFINE_bool(
+        name="sortagrad", default=True,
+        help=flags_core.help_wrap(
+            "If true, sort examples by audio length and perform no "
+            "batch_wise shuffling for the first epoch."))
+
+    flags.DEFINE_integer(
+        name="sample_rate", default=16000,
+        help=flags_core.help_wrap("The sample rate for audio."))
+
+    flags.DEFINE_integer(
+        name="window_ms", default=20,
+        help=flags_core.help_wrap("The frame length for spectrogram."))
+
+    flags.DEFINE_integer(
+        name="stride_ms", default=10,
+        help=flags_core.help_wrap("The frame step."))
+
+    flags.DEFINE_string(
+        name="vocabulary_file", default=_DEFAULT_CHARACTERS_FILE,
+        help=flags_core.help_wrap("The file path of vocabulary file."))
+
+    # RNN related flags
+    flags.DEFINE_integer(
+        name="rnn_hidden_size", default=800,
+        help=flags_core.help_wrap("The hidden size of RNNs."))
+
+    flags.DEFINE_integer(
+        name="rnn_hidden_layers", default=5,
+        help=flags_core.help_wrap("The number of RNN layers."))
+
+    flags.DEFINE_bool(
+        name="use_bias", default=True,
+        help=flags_core.help_wrap("Use bias in the last fully-connected layer"))
+
+    flags.DEFINE_bool(
+        name="is_bidirectional", default=True,
+        help=flags_core.help_wrap("If rnn unit is bidirectional"))
+
+    flags.DEFINE_enum(
+        name="rnn_type", default="gru",
+        enum_values=deep_speech.SUPPORTED_RNNS.keys(),
+        case_sensitive=False,
+        help=flags_core.help_wrap("Type of RNN cell."))
+
+    # Training related flags
+    flags.DEFINE_float(
+        name="learning_rate", default=5e-4,
+        help=flags_core.help_wrap("The initial learning rate."))
+
+    # Evaluation metrics threshold
+    flags.DEFINE_float(
+        name="wer_threshold", default=None,
+        help=flags_core.help_wrap(
+            "If passed, training will stop when the evaluation metric WER is "
+            "greater than or equal to wer_threshold. For libri speech dataset "
+            "the desired wer_threshold is 0.23 which is the result achieved by "
+            "MLPerf implementation."))
+
+
+def main(_):
+    with logger.benchmark_context(flags_obj):
+        run(flags_obj)
+
+
+if __name__ == "__main__":
+    define_deep_speech_flags()
+    flags_obj = flags.FLAGS
+    absl_app.run(main)
